3b1c2307a3d3bee1a61d1dbba94191802ddde2d4
Damjan <dakijovanovic03@gmail.com>
2024-03-05 16:03:16 +0100

    Add balance, wallet and send(partially)

--- app.js
+++ app.js
@@ -1,25 +1,59 @@
+const { getBlockNumbers, createSigner, createProvider } = require("./script.js")
 const express = require('express')
+const { ethers } = require("ethers");
 const app = express()
 const port = 3000
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || ""
 
-app.get('/', (req, res) => {
-  res.send('GET request\n')
+app.get('/blocks', async (req, res) => {
+  const result = await getBlockNumbers()
+  res.send(result)
+  //res.send('GET request\n')
 })
 
-app.post('/', (req, res) => {
-    res.send('POST request\n')
+app.post('/wallet', async (req, res) => {
+  try {
+      const provider = await createProvider()
+      const wallet= await createSigner(PRIVATE_KEY, provider)
+
+      console.log("Wallet Address: ", wallet.address);
+      console.log("Provider: ", wallet.provider)
+
+      res.send(`privateKey: ${wallet.privateKey}`)
+  } catch (error) {
+      res.status(500).send({ error: error.message })
+  }
 })
 
-app.all('/secret', (req, res, next) => {
-    console.log('Accessing the secret section ...')
-    res.send("iz secreta")
-    next()
+app.get('/balance', async (req, res) => {
+  try {
+    const provider = await createProvider()
+    const wallet = await createSigner(PRIVATE_KEY, provider)
+    
+    const balance = await provider.getBalance(wallet.address)
+    console.log(balance.toString())
+    
+    const newBalance = ethers.formatEther(balance)
+    console.log(newBalance)
+
+    res.send({ balance: newBalance });
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
 })
 
-app.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at /user')
-})
+app.post('/send', async (req, res) => {
+  try {
+    const { to, value } = { to: "0x1234567890123456789012345678901234567890", value: "200000000000000000" }
+    const wallet = await createSigner(PRIVATE_KEY)
+    const transaction = await wallet.sendTransaction({ to, value })
+    res.send({ transactionHash: transaction.hash })
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Slusa se port ${port}`)
+  console.log(`Listening to port ${port}`)
 })
--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 const { Network, Alchemy } = require("alchemy-sdk");
-require("dotenv").config();
 const { ethers } = require("ethers");
+require("dotenv").config();
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || ""
 
-// Optional Config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
-    apiKey: ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
-    network: Network.ETH_MAINNET, // Replace with your network.
+    apiKey: ALCHEMY_API_KEY, 
+    network: Network.ETH_SEPOLIA, 
 };
 const alchemy = new Alchemy(settings);
 
-//Initialize provider using HTTP endpoint URL
-const providerUrl = `https://eth-mainnet.alchemyapi.io/v2/${settings.apiKey}`;
-const provider = new ethers.JsonRpcProvider(providerUrl)
+async function createProvider() {
+  const providerUrl = `https://eth-sepolia.g.alchemy.com/v2/${settings.apiKey}`
+  const provider = new ethers.JsonRpcProvider(providerUrl)
 
-async function main() {
-    const latestBlock = await alchemy.core.getBlockNumber();
-    console.log("The latest block number is", latestBlock);
-    
-
-  const blockNumber = await provider.getBlockNumber();
-  console.log("The latest block number using ethers.js provider is", blockNumber);
+  return provider;
 }
 
-main();
+async function getBlockNumbers() {
+    const provider = await createProvider()
+    
+    const latestBlock = await alchemy.core.getBlockNumber()
+    const blockNumber = await provider.getBlockNumber()
+
+    return { latestBlock, blockNumber };
+}
+
+async function createSigner(privateKey) {
+  const wallet = new ethers.Wallet(privateKey, createProvider());
+
+  return wallet;
+}
+
+module.exports = { getBlockNumbers, createSigner, createProvider }
