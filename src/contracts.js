4e9478885ccbb3c49f6f017279a75b64db6aa99a
Damjan <dakijovanovic03@gmail.com>
2024-03-08 15:37:52 +0100

    MongoDB database filling and saving data in json file

--- src/contracts.js
+++ src/contracts.js
@@ -1,30 +1,86 @@
 const ethers = require("ethers");
 const ABI = require("../abi/abi.json");
-const { getBlockNumbers, createSigner, createProvider, createWebSocketProvider } = require("./script.js")
+const fs = require('fs');
+const { SwapEvent, connect } = require('./mongodb.js');
+const { createWebSocketProvider } = require("./script.js");
 require("dotenv").config();
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || ""
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
+const filePath = '../swapEventDB.json';
 
-
-async function getSwap(){
+async function getSwap() {
     const usdcAddress = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852"; // USDC Contract
     const provider = await createWebSocketProvider(ALCHEMY_API_KEY);
-    
-
     const contract = await new ethers.Contract(usdcAddress, ABI, provider);
 
-    contract.on("Swap", (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
-        let swapEvent = {
-            from: sender,
-            amount0In: amount0In,
-            amount1In: amount1In,
-            amount0Out: amount0Out,
-            amount1Out: amount1Out,
-            to: to,
+    connect();
+    let firstObject = true;
+
+    // Read the content of the JSON file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+        } else {
+            // Remove the last character from the data
+            let newData = data.slice(0, -2);
+            fs.stat(filePath, (err, stats) => {
+                if (err) {
+                    console.error('Error checking file stats:', err);
+                } else {
+                    if (stats.size === 0) {
+                        newData += "[\n";
+                    } else {
+                        newData += ",\n";
+                    }
+
+                    // Write the modified content back to the file
+                    fs.writeFile(filePath, newData, 'utf8', (err) => {
+                        if (err) {
+                            console.error('Error writing file:', err);
+                        } else {
+                            console.log('Last character removed from the JSON file.');
+                        }
+                    });
+                }
+            });
         }
-        console.log(swapEvent);
+    });
+
+    contract.on("Swap", async (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
+        try {
+            const objSwap = new SwapEvent({
+                from: sender,
+                amount0In: Number(amount0In),
+                amount1In: Number(amount1In),
+                amount0Out: Number(amount0Out),
+                amount1Out: Number(amount1Out),
+                to,
+            });
+
+            let jsonStr = JSON.stringify(objSwap, null, 2);
+
+            if (!firstObject) {
+                jsonStr = ',\n' + jsonStr;
+            } else {
+                firstObject = false;
+            }
+
+            fs.appendFileSync(filePath, jsonStr, 'utf8');
+
+            objSwap.save();
+            console.log("Swap event saved to the database:", objSwap);
+        } catch (error) {
+            console.error("Error saving swap event:", error);
+        }
+    });
+
+    process.on('SIGINT', () => {
+        console.log('Received SIGINT signal. Closing the file.');
+        fs.appendFileSync(filePath, '\n]', 'utf8'); // Write closing square bracket and new line at the end of the file
+        console.log("Finished writing to file.");
+        process.exit(0); // Exit the process after cleanup
     });
 
     console.log("Initialized event listener");
 }
 
-getSwap()
+getSwap();
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,43 +1,18 @@
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const password = process.env.PASSWORD || ""
-
-// const uri = `mongodb+srv://Damjan:123@testcluster1.l2wbbey.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster1`;
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-
-
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// getting-started.js
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
-
-async function main() {
-    await mongoose.connect(`mongodb+srv://Damjan:123@testcluster1.l2wbbey.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster1`);
-    console.log("Successfully connected to the MongoDB")
-
-    // const kittySchema = new mongoose.Schema({
-    //     from: String,
-    //     to: String,
-    // });
+async function connect() {
+    await mongoose.connect('mongodb+srv://Damjan:123@testcluster1.l2wbbey.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster1');
+    console.log('Connected to the MongoDB successfully');
 }
+
+const swapEventSchema = new mongoose.Schema({
+    from: String,
+    amount0In: Number,
+    amount1In: Number,
+    amount0Out: Number,
+    amount1Out: Number,
+    to: String,
+});
+const SwapEvent = mongoose.model('SwapEvent', swapEventSchema);
+
+module.exports = {SwapEvent, connect};
